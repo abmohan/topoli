408e003b8177b5c0a021bafdacc45dfbca4161a9
Ashwin Balamohan <abmohan@gmail.com>
2014-11-09 18:58:31 -0500

    Renamed node's app.js to index.js

---
+++ index.js
@@ -0,0 +1,54 @@
+
+// set up =======================================
+var express = require('express');
+var mongoose = require('mongoose');
+var TorontoWard = require('./models/TorontoWard');
+
+// configuration ================================
+// mongo
+var uristring = process.env.MONGOLAB_URI || 'mongodb://localhost/topoli';
+mongoose.connect(uristring, function (err) {
+  if (err) {
+    console.log("ERROR connection to", uristring, ":", err);
+    return;
+  }
+
+  console.log("Succussfully connected to:", uristring);
+});
+
+// express
+var app = express();
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+
+// routes 
+app.get('/', function (req, res) {
+  console.log("Request received at '/'", req.headers);
+  res.sendfile('./public/index.html');
+});
+
+app.get('/api', function (req, res) {
+  console.log("Request received at '/api'", req.headers);
+  res.send("topoli API running.");
+});
+
+app.get('/api/wards', function (req, res) {
+
+  console.log("Request received at '/api/wards'", req.headers);
+
+  TorontoWard.find(function (err, wards) {
+    if (err) {
+      res.send(err);
+    }
+
+    res.json(wards);
+  });
+
+});
+
+// listen =======================================
+
+app.listen(app.get('port'), function () {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
